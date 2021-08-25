81b176e23fb3a4ba85a45de0c7a9b29cc5398640
unknown <gitmjreilly@gmail.com>
2021-08-25 16:10:43 -0400

    Lots of changes. This version gets hashes in the destination folder FIRST. This way, if the program is run more than once on the same source and destination folders, it will have hashes of already copied files so it won't copy them again.

--- no_dup_copier.py
+++ no_dup_copier.py
@@ -15,7 +15,7 @@ DESTINATION_BASE_DIR = Path("c:/destination_pictures")
 CHUNK_SIZE = 8192 * 512
 
 
-def get_file_checksum(filename : Path) -> str:
+def get_file_hash(filename : Path) -> str:
     """ Given a filename, return a hexdigest string """
     with open(filename, "rb") as f:
         hash = hashlib.blake2b()
@@ -58,7 +58,7 @@ def folders_are_mutually_relative(f1 : Path, f2 : Path) -> int:
     return(0)
 
 
-def get_relative_from_absolute(base_dir : Path, absolute_dir : Path) -> Path:
+def get_relative_path_from_absolute(base_dir : Path, absolute_dir : Path) -> Path:
     """ 
     Given a base dir like  'c:/base'
 
@@ -82,6 +82,27 @@ def get_relative_from_absolute(base_dir : Path, absolute_dir : Path) -> Path:
 
     return(relative_dir)
 
+def get_hashes_from_dir(the_base_dir : Path) -> Dict[str, Path]:
+    """ 
+    given the_base_dir
+    return a dict keyed by hashes and whose values are the full names of 
+    every file in the_base_dir (including all sub dirs)
+    """
+    file_info_by_checksum : Dict[str, Path] = {}
+
+    for (absolute_dir, _, relative_filenames) in os.walk(the_base_dir):
+
+        # We work with Path's not the strings returned by os.walk...
+        absolute_dir = Path(absolute_dir)
+
+        for relative_filename in relative_filenames:
+                     
+            absolute_filename = absolute_dir / relative_filename
+            if absolute_filename.exists():
+                check_sum = get_file_hash(absolute_filename)
+                file_info_by_checksum[check_sum] = absolute_filename
+
+    return(file_info_by_checksum)
 
 def main():
     print("No duplicate file copier.")
@@ -91,7 +112,7 @@ def main():
     num_duplicates = 0
     num_symlinks = 0
     num_non_regular = 0
-    copy_info_by_checksum : Dict[str, Path] = {}
+    copy_info_by_hash : Dict[str, Path] = {}
 
     n = folders_are_mutually_relative(SOURCE_BASE_DIR, DESTINATION_BASE_DIR)
     if (n == 1):
@@ -101,9 +122,21 @@ def main():
         print("ERROR Destination folder is within the Source folder; not allowed!")
         sys.exit(1)
 
- 
+    # Get all of the hashes for the files in the destination folder
+    # The idea is we may have run this program previously and 
+    # copied files into the destination.
+    # If so, we want to know about those files so we don't copy
+    # them (or files with same checksum) again.
+    print(f"Scanning {DESTINATION_BASE_DIR} so we know what files were already copied...")
+    copy_info_by_hash = get_hashes_from_dir(DESTINATION_BASE_DIR)
+    print(f"There were {len(copy_info_by_hash)} in found.")
 
 
+    # Recursively walk the source dir
+    # Get hashes for all files and then only copy REGULAR files
+    # whose hashes we have not already seen
+    # And once we do copy a file, note its hash so we won't copy
+    # future files with same hash
     for (absolute_source_dir, _, relative_filenames) in os.walk(SOURCE_BASE_DIR):
 
         # We work with Path's not the strings returned by os.walk...
@@ -111,7 +144,7 @@ def main():
 
         print("\n+++++++++++++++++++++")
         
-        relative_dir = get_relative_from_absolute(SOURCE_BASE_DIR, absolute_source_dir)
+        relative_dir = get_relative_path_from_absolute(SOURCE_BASE_DIR, absolute_source_dir)
 
         absolute_destination_dir = DESTINATION_BASE_DIR /  relative_dir
         absolute_destination_dir.mkdir(parents = True, exist_ok=True)
@@ -135,18 +168,26 @@ def main():
                 continue
 
 
-            check_sum = get_file_checksum(absolute_source_name)
+            hash = get_file_hash(absolute_source_name)
             print("  DEST  Name    [%s]" % absolute_destination_name)
 
-            if check_sum not in copy_info_by_checksum:
-                print("  INFO - We have not seen this file before; will copy it")
-                shutil.copy(str(absolute_source_name), str(absolute_destination_name))
-                copy_info_by_checksum[check_sum] = absolute_source_name
-                num_copied += 1
-            else:
+
+
+            # Check for easy case where file has already been seen
+            if hash  in copy_info_by_hash:
                 print("  INFO We already saw %s - will NOT copy" % absolute_source_name)
-                print("  It was copied as [%s]" % (copy_info_by_checksum[check_sum]))
+                print("  It was copied as [%s]" % (copy_info_by_hash[hash]))
                 num_duplicates += 1
+                continue
+
+
+            # We have not seen this file before (at least not during this run of the program!)
+            # Now what?
+            print("  INFO - We have not seen this file before; will copy it")
+            shutil.copy(str(absolute_source_name), str(absolute_destination_name))
+            copy_info_by_hash[hash] = absolute_source_name
+            num_copied += 1
+        
 
     print("Num files seen : %d" % (num_files_seen))
     print("Num copied     : %d" % (num_copied))
